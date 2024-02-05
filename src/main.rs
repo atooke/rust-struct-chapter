236229daf9de29339de09e28fd116796501cc73c
Andy Tooke <andy.tooke@mckesson.com>
2024-02-05 07:52:11 -0500

    create usr

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,16 @@ struct User {
 }
 
 fn main() {
-    println!("Hello, world!");
+    // create new user:
+    let mut user1 = User {
+        active: true,
+        username: String::from("some name"),
+        email: String::from("some_name@gmail.com")
+        sign_in_count: 1,
+    };
+    //change value of user email - object must be mutable
+    user1.email = String::from("new_email@gmail.com");
+    
+    
+
 }
